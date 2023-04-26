5f093b291fd0d2d21c01d719710163a61cf85aa2
Krzysztof Wrona <krzychuwr1@gmail.com>
2023-04-26 14:28:25 +0100

    Adjust bullets

--- Assets/BulletBehaviour.cs
+++ Assets/BulletBehaviour.cs
@@ -1,17 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    private void OnTriggerEnter(Collider other)
+    {
+        var tileCollisionHandler = other.gameObject.GetComponent<TileToggleHelper>();
+        if(tileCollisionHandler != null) {
+            tileCollisionHandler.Toggle();
+            Destroy(gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         var tileCollisionHandler = collision.gameObject.GetComponent<TileToggleHelper>();
         if(tileCollisionHandler != null) {
             tileCollisionHandler.Toggle();
-        }
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Grid"))
-        {
             Destroy(gameObject);
         }
     }
