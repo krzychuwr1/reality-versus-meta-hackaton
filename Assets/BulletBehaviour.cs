6ae555e2bba11ed1c0b4b122528deaba618825a7
Krzysztof Wrona <krzychuwr1@gmail.com>
2023-04-26 16:40:44 +0100

    Add a (pretty basic) trail

--- Assets/BulletBehaviour.cs
+++ Assets/BulletBehaviour.cs
@@ -1,33 +1,49 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using App.Scripts.Utilities;
 using Photon.Pun;
 using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _trail;
+
+    private GameObject _trailInstance;
+
     private void Start()
     {
         this.ExecuteDelayed(() =>
         {
             if (gameObject) PhotonNetwork.Destroy(gameObject);
-        }, 5.0f);
+        }, 0.3f);
+        var bulletTransform = transform;
+        _trailInstance = Instantiate(_trail, bulletTransform.position, bulletTransform.rotation);
+    }
+
+    private void Update()
+    {
+        var bulletTransform = transform;
+        _trailInstance.transform.position = bulletTransform.position;
+        _trailInstance.transform.rotation = bulletTransform.rotation;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(_trailInstance.gameObject, 0.1f);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         var tileCollisionHandler = other.gameObject.GetComponent<TileToggleHelper>();
-        if(tileCollisionHandler != null) {
-            tileCollisionHandler.Toggle();
-            PhotonNetwork.Destroy(gameObject);
-        }
+        if (tileCollisionHandler == null) return;
+        tileCollisionHandler.Toggle();
+        PhotonNetwork.Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         var tileCollisionHandler = collision.gameObject.GetComponent<TileToggleHelper>();
-        if(tileCollisionHandler != null) {
+        if (tileCollisionHandler != null) {
             tileCollisionHandler.Toggle();
             PhotonNetwork.Destroy(gameObject);
         }
