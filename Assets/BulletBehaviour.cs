9d139162c47a982f1318f73337142b0f747396bb
Moritz Loos <moritzloos@gmx.de>
2023-04-26 15:09:19 +0200

    add toggle tiles to bullet behavior.

--- Assets/BulletBehaviour.cs
+++ Assets/BulletBehaviour.cs
@@ -6,6 +6,10 @@ public class BulletBehaviour : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        var tileCollisionHandler = collision.gameObject.GetComponent<TileToggleHelper>();
+        if(tileCollisionHandler != null) {
+            tileCollisionHandler.Toggle();
+        }
         if (collision.gameObject.layer == LayerMask.NameToLayer("Grid"))
         {
             Destroy(gameObject);
